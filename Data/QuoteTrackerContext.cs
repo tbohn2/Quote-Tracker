e087c3a2ef5054db172603d3652d3a01a3de2dc4
tbohn2 <tanner.bohn@yahoo.com>
2025-04-08 23:00:17 -0700

    Set up DB context and relationships

---
+++ Data/QuoteTrackerContext.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Quote_Tracker.Models;
+using System.Reflection;
+
+namespace Quote_Tracker.Data
+{
+    public class Quote_Tracker_Context : DbContext
+    {
+        public Quote_Tracker_Context(DbContextOptions<Quote_Tracker_Context> options) : base(options) { }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<QuoteTopic> QuoteTopics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Quote>()
+                .Property(q => q.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            modelBuilder.Entity<Quote>()
+                .HasOne(q => q.Book)
+                .WithMany(b => b.Quotes)
+                .HasForeignKey(q => q.BookId);
+
+
+            modelBuilder.Entity<QuoteTopic>()
+                .HasKey(qt => new { qt.QuoteId, qt.TopicId });
+
+            modelBuilder.Entity<QuoteTopic>()
+                .HasOne(qt => qt.Quote)
+                .WithMany(q => q.QuoteTopics)
+                .HasForeignKey(qt => qt.QuoteId);
+
+            modelBuilder.Entity<QuoteTopic>()
+                .HasOne(qt => qt.Topic)
+                .WithMany(t => t.QuoteTopics)
+                .HasForeignKey(qt => qt.TopicId);
+        }
+
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -6,5 +6,6 @@ namespace Quote_Tracker.Models
         public int Id { get; set; }
         public required string Title { get; set; }
         public string? Author { get; set; }
+        public ICollection<Quote> Quotes { get; set; } = new List<Quote>();
     }
 }
