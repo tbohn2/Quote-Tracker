0383291dc5771b4a0dea3ddfc29e154322769bb0
tbohn2 <tanner.bohn@yahoo.com>
2025-05-02 17:48:14 -0700

    Function to render quotes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -34,14 +34,36 @@ function renderBooks() {
 }
 
 function renderQuotes() {
-    console.log(quotes);
+    quotes.forEach(quote => {
+        const bookId = quote.bookId;
+
+        const topicsDisplay = quote.topics.map(topic => {
+            const display = `<p>${topic}</p>`;
+            return display;
+        }).join('');
+
+        const display = `
+            <div id=${quote.id} class="d-flex quote-row">
+            <div class="col-3 text-start ps-1">
+                <p>${quote.chapter} : ${quote.verse}</p>
+                <p>${quote.page}</p>
+            </div>
+            <p class="col-6 text-start">${quote.text} by ${quote.person}</p>
+            <div class="col-3 text-start">
+                ${topicsDisplay}
+            </div>
+        </div>`
+
+        $(`book${bookId}`).append(display);
+    });
+
 }
 
 
 $(document).ready(async function () {
     await fetchAndSetBooks();
-    // await fetchAndSetQuotes();
+    await fetchAndSetQuotes();
     renderBooks();
-    // renderQuotes();
+    renderQuotes();
 }
 )
