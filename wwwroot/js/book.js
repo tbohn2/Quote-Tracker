785070fd95b27bed9a4b9b760496a514f7706794
tbohn2 <tanner.bohn@yahoo.com>
2025-05-08 16:12:08 -0700

    Books are reordered and saved by drag and drop

---
+++ wwwroot/js/book.js
@@ -0,0 +1,61 @@
+
+let draggedItem = null;
+let start = 0;
+
+async function saveChangedReorder() {
+    const bookOrder = [];
+
+    $('#book-list .book-item').each(function (i) {
+        if (i >= start) {
+            bookOrder.push({
+                Id: $(this).data('id'),
+                PriorityIndex: i
+            });
+        }
+    });
+
+    const response = await fetch('api/book/reorder', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bookOrder)
+    })
+
+    const message = await response.text();
+    console.log(message);
+
+    $('#save-reorder').remove();
+}
+
+$('#book-list').on('dragstart', '.book-item', function (e) {
+    draggedItem = $(this);
+    draggedItem.css('opacity', 0.5);
+});
+
+$('#book-list').on('dragend', '.book-item', function (e) {
+    $(this).css('opacity', '');
+});
+
+$('#book-list').on('dragover', function (e) {
+    e.preventDefault();
+});
+
+$('#book-list').on('drop', '.book-item', function (e) {
+    e.preventDefault();
+
+    if (draggedItem[0] !== this) {
+        $('#books').append(`<button id="save-reorder" class="button primary my-3">Save Changes</button>`)
+
+        $('#save-reorder').on('click', saveChangedReorder)
+
+        const draggedIndex = draggedItem.index();
+        const targetIndex = $(this).index();
+
+        if (draggedIndex < targetIndex) {
+            $(this).after(draggedItem);
+        } else {
+            $(this).before(draggedItem);
+        }
+    }
+});
