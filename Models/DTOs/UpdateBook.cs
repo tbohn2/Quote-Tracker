5f97669a0643ec2d729f28772ade1e99a3e49ec8
tbohn2 <tanner.bohn@yahoo.com>
2025-05-12 17:49:31 -0700

    Removed PriorityIndex from UpdateBook DTO

--- Models/DTOs/UpdateBook.cs
+++ Models/DTOs/UpdateBook.cs
@@ -5,6 +5,5 @@ namespace Quote_Tracker.Models
         public required int Id { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
-        public int? PriorityIndex { get; set; }
     }
 }
