5d90117636d37bf24de95711cff505f9c4a99a87
tbohn2 <tanner.bohn@yahoo.com>
2025-05-15 19:04:01 -0700

    Books rerender upon successful fetch requests; reorder indices handled properly

--- wwwroot/js/book.js
+++ wwwroot/js/book.js
@@ -1,6 +1,7 @@
 
 let draggedItem = null;
-let start = 0;
+let start = $("#book-list .book-item").length - 1;
+let end = 0;
 let newBook = {
     Title: "",
     Author: "",
@@ -8,33 +9,45 @@ let newBook = {
 }
 
 async function saveChangedReorder() {
-    const bookOrder = [];
+    const booksToReorder = [];
 
-    $('#book-list .book-item').each(function (i) {
-        if (i >= start) {
-            bookOrder.push({
-                Id: $(this).data('id'),
-                PriorityIndex: i
-            });
+    for (let i = start; i < end + 1; i++) {
+        const id = $("#book-list .book-item").eq(i).data("id");
+
+        booksToReorder.push({
+            Id: id,
+            PriorityIndex: i,
+        });
+    }
+
+    try {
+        const response = await fetch('api/book/reorder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(booksToReorder)
+        })
+
+        const data = await response.json();
+        if (response.ok) {
+            $('#save-reorder').remove();
+            $('#book-list').remove();
+            renderBookList(data);
+            start = 0;
+            end = $("#book-list .book-item").length - 1;
+        } else {
+            console.error(data);
         }
-    });
-
-    const response = await fetch('api/book/reorder', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bookOrder)
-    })
-
-    const message = await response.text();
-    console.log(message);
-
-    $('#save-reorder').remove();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function saveNewBook(e) {
     e.preventDefault()
+
+    console.log(newBook);
 
     try {
         const response = await fetch('api/book/', {
@@ -51,7 +64,8 @@ async function saveNewBook(e) {
         } else {
             $('#addBook').modal('hide');
             resetFormState();
-            console.log(data);
+            $('#book-list').remove();
+            renderBookList(data);
         }
     } catch (error) {
         console.error(error)
@@ -76,39 +90,62 @@ function resetFormState() {
     $('select[name="PriorityIndex"]').val("0");
 }
 
+function renderBookList(books) {
+    const booksDisplay = `
+    <ul id="book-list" class="col-6 p-0">
+        ${books.map(book =>
+        `<li class="book-item text-center p-2" data-id="${book.id}" data-pi="${book.priorityIndex}" draggable="true">
+            <a href="/BookPage/Details/${book.id}">
+               ${book.title}
+            </a>
+        </li>`).join('')}
+    </ul>
+    `
+
+    $('#books').append(booksDisplay);
+    applyDragEventListeners();
+};
+
 $('#create-book-form').on('change', updateForm);
 $('#create-book-form').on('submit', saveNewBook);
 $('#close-book-modal').on('click', resetFormState);
 
-$('#book-list').on('dragstart', '.book-item', function (e) {
-    draggedItem = $(this);
-    draggedItem.css('opacity', 0.5);
-    $('#save-reorder').remove();
-});
+function applyDragEventListeners() {
+    $('#book-list').on('dragstart', '.book-item', function (e) {
+        draggedItem = $(this);
+        draggedItem.css('opacity', 0.5);
+    });
 
-$('#book-list').on('dragend', '.book-item', function (e) {
-    $(this).css('opacity', '');
-});
+    $('#book-list').on('dragend', '.book-item', function (e) {
+        $(this).css('opacity', '');
+    });
 
-$('#book-list').on('dragover', function (e) {
-    e.preventDefault();
-});
+    $('#book-list').on('dragover', function (e) {
+        e.preventDefault();
+    });
 
-$('#book-list').on('drop', '.book-item', function (e) {
-    e.preventDefault();
+    $('#book-list').on('drop', '.book-item', function (e) {
+        e.preventDefault();
 
-    if (draggedItem[0] !== this) {
-        $('#books').append(`<button id="save-reorder" class="button primary my-3">Save Changes</button>`)
+        if (draggedItem[0] !== this) {
+            if (!$('#save-reorder').html()) {
+                $('#books').append(`<button id="save-reorder" class="button primary my-3">Save Changes</button>`)
+                $('#save-reorder').on('click', saveChangedReorder)
+            }
 
-        $('#save-reorder').on('click', saveChangedReorder)
+            const draggedIndex = $("#book-list .book-item").index($(draggedItem));
+            const targetIndex = $("#book-list .book-item").index($(this));
 
-        const draggedIndex = draggedItem.index();
-        const targetIndex = $(this).index();
+            start = Math.min(Math.min(draggedIndex, targetIndex), start);
+            end = Math.max(Math.max(draggedIndex, targetIndex), end);
 
-        if (draggedIndex < targetIndex) {
-            $(this).after(draggedItem);
-        } else {
-            $(this).before(draggedItem);
+            if (draggedIndex < targetIndex) {
+                $(this).after(draggedItem);
+            } else {
+                $(this).before(draggedItem);
+            }
         }
-    }
-});
+    });
+}
+
+applyDragEventListeners();
